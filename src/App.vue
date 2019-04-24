a53b9d15a463bd1ed5d1c9d77feadcd9930df237
Alana Rodrigues <alana@e-midia.com>
2019-04-24 17:45:08 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <header>
+      <ul>
+        <li>
+          <router-link to="/">Home</router-link>
+        </li>
+        <li>
+          <router-link to="/teste">Teste</router-link>
+        </li>
+      </ul>
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+a{
+  color: #fff;
+  text-decoration: none;
+}
+ul{
+  float: left;
+}
+ul li{
+  padding: 10px;
+  list-style: none;
+  display: inline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  float: left;
+  width: 100%;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+
+header {
+  float: left;
+  width: 100%;
+  margin: 0;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+</style>
